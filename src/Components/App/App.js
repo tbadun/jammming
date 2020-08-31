4246aa1e8682c32bf5963e458544ed1836ca8635
tbadun <58403128+tbadun@users.noreply.github.com>
2020-08-31 09:33:07 -0400

    search complete

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import SearchBar from '../SearchBar/SearchBar';
 import SearchResults from '../SearchResults/SearchResults';
